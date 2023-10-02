5b1b4f59b9432109f33bf303a64c61ca6eaabcac
minji <321963741@naver.com>
2023-10-02 14:55:07 +0900

    Feat: appModule에서 다른 modules들 import 정의해주기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from "@nestjs/common";
+import { UserModule } from "./modules/user/user.module";
+import { CompanyModule } from "./modules/company/company.module";
+import { JobPostingModule } from "./modules/job_posting/job_posting.module";
+import { ApplicationHistoryModule } from "./modules/application_history/application_history.module";
 
-@Module({})
+@Module({
+    imports: [
+        UserModule,
+        CompanyModule,
+        JobPostingModule,
+        ApplicationHistoryModule,
+    ],
+})
 export class AppModule {}
