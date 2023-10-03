b1933dd6624656f9799a2b6661a204631e209559
minji <321963741@naver.com>
2023-10-03 23:05:12 +0900

    Feat(swagger) : initialize swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,6 +17,17 @@ async function bootstrap() {
             forbidNonWhitelisted: true,
         }),
     );
+
+    //  Initialize Swagger and APP
+    if (process.env.NODE_ENV !== "prd") {
+        const swaggerConfig = new DocumentBuilder()
+            .setTitle("Wanted Preonboarding backend API")
+            .setDescription("원티드 프리온보딩 백엔드 사전과제 김민지")
+            .setVersion("1.0")
+            .build();
+        const document = SwaggerModule.createDocument(app, swaggerConfig);
+        SwaggerModule.setup("api-docs", app, document);
+    }
 
     // app listen
     await app.listen(process.env.PORT || 8000);
