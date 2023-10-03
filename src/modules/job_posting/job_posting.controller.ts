fcaf201d4f6853935432bd04c7853e39372137cf
minji <321963741@naver.com>
2023-10-03 12:16:43 +0900

    Refactor(controller) : rename createPosting controller to createJobPosting

--- src/modules/job_posting/job_posting.controller.ts
+++ src/modules/job_posting/job_posting.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Patch, Post } from "@nestjs/common";
 import { JobPostingsService } from "./job_posting.service";
 import { CreateJobPostingDTO } from "./dto/create-posting.dto";
 
@@ -10,7 +10,7 @@ export class JobPostingsController {
      * 채용 공고 등록
      **/
     @Post("/")
-    async createPosting(@Body() posting: CreateJobPostingDTO) {
+    async createJobPosting(@Body() posting: CreateJobPostingDTO) {
         return this.jobPostingsService.createJobPosting(posting);
     }
 }
