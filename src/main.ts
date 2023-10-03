de9f0b29c50c4d859838c393e7d4ab5e894f3d44
minji <321963741@naver.com>
2023-10-03 12:12:56 +0900

    Feat(dto): configure global ValidationPipe in Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -7,6 +8,17 @@ async function bootstrap() {
     // middleware
     app.setGlobalPrefix("/api/v1");
 
+    // Set global pipes
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    // app listen
     await app.listen(process.env.PORT || 8000);
 }
+
 bootstrap();
